4503723f8da889ffaf79e4c2340d10ad3b8a4740
marcosabella <ms_abella@hotmail.com>
2026-07-08 08:45:23 -0300

    commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,9 @@ const config: CapacitorConfig = {
   appId: 'com.sistemapedidos.web',
   appName: 'Sistema Pedidos',
   webDir: 'dist',
+  android: {
+    adjustMarginsForEdgeToEdge: 'force',
+  },
   server: {
     androidScheme: 'https',
   },
